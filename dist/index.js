bcadfdeb051808e726dfd344ddb6decf2d4eca9f
Harsh Ghodkar <harshghodkar@gmail.com>
2021-09-16 16:15:16 +0530

    Added GWOC Changes

--- dist/index.js
+++ dist/index.js
@@ -40,7 +40,7 @@ const run = async () => {
        } else if (event === 'issues') {
          message = issue_message;
 
-         const labels = ["DCP21"];
+         const labels = ["GWOC"];
 
          // Adding the labels present in the array.
          octokit.issues.addLabels({
